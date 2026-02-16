d11ec21c0036f0c5c4196cd6fa760933b1df2523
tatata812 <75353660+tatata812@users.noreply.github.com>
2026-02-16 21:31:33 +0900

    修正

--- inc/functions/base/news.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-add_action('pre_get_posts', function ($q) {
-    if (is_admin() || !$q->is_main_query()) return;
-    if ($q->is_post_type_archive('news')) {
-        $q->set('posts_per_page', 10);
-    }
-});
